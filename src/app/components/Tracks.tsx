d00197fb6ddee099ddaee76e970396c9dddced79
Daniel Sil <daniel.sil@kiwi.com>
2025-01-02 00:16:34 +0000

    fix: not show people in future dates

--- src/app/components/Tracks.tsx
+++ src/app/components/Tracks.tsx
@@ -28,9 +28,9 @@ export default function Tracks({ scheduledDay, playlistTracks }: TracksProps) {
     const isTrack3Future = Time.isFuture(scheduledDay.allRules[2].date);
 
     tracks = [
-      <Track key={"track-1"}  date={isTrack1Future ? "???" : formatDate(scheduledDay.allRules[0].date)} player={scheduledDay.allRules[0].player} songId={songId(playlistTracks, scheduledDay.playlistIndices[0])}/>,
-      <Track key={"track-2"}  date={isTrack2Future ? "???" : formatDate(scheduledDay.allRules[1].date)} player={scheduledDay.allRules[1].player} songId={songId(playlistTracks, scheduledDay.playlistIndices[1])} />,
-      <Track key={"track-3"}  date={isTrack3Future ? "???" : formatDate(scheduledDay.allRules[2].date)} player={scheduledDay.allRules[2].player} songId={songId(playlistTracks, scheduledDay.playlistIndices[2])} />,
+      <Track key={"track-1"}  date={isTrack1Future ? "???" : formatDate(scheduledDay.allRules[0].date)} player={isTrack1Future ? "???" : scheduledDay.allRules[0].player} songId={songId(playlistTracks, scheduledDay.playlistIndices[0])}/>,
+      <Track key={"track-2"}  date={isTrack2Future ? "???" : formatDate(scheduledDay.allRules[1].date)} player={isTrack2Future ? "???" : scheduledDay.allRules[1].player} songId={songId(playlistTracks, scheduledDay.playlistIndices[1])} />,
+      <Track key={"track-3"}  date={isTrack3Future ? "???" : formatDate(scheduledDay.allRules[2].date)} player={isTrack3Future ? "???" : scheduledDay.allRules[2].player} songId={songId(playlistTracks, scheduledDay.playlistIndices[2])} />,
     ];
   }
 
